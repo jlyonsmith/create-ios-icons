58f905f8f0f01d158a0c42e6ad3d9e8885e8bdd8
John Lyon-Smith <john@lyon-smith.org>
2021-02-20 09:19:33 -0800

    Version 1.0.0+20210320.2-main

--- dist/version.js
+++ dist/version.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fullVersion = exports.shortVersion = void 0;
 exports.shortVersion = "1.0.0";
-exports.fullVersion = "1.0.0+20210316.1";
+exports.fullVersion = "1.0.0+20210320.2";
 //# sourceMappingURL=version.js.map
